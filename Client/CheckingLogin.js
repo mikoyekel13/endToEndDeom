0e33d395a75beb02fb2b4d7e25f6e430c9cab02a
mikoyekel13 <mikoyekel@hilma.tech>
2023-10-31 15:07:12 +0200

    delete something

--- Client/CheckingLogin.js
+++ Client/CheckingLogin.js
@@ -1,15 +1,3 @@
-
-
-
-const submitbtn = document.querySelector('#submitbtn');
-function loginTemp(){
-    const loginTemp = document.querySelector('#loginTemp');
-    const login= loginTemp.content.cloneNode(true);
-
-    document.body.appendChild(login);
-    
-    
-}
 function checkValid(){
     console.log(users);
     const pwrd = document.querySelector('#pwrd').value;
