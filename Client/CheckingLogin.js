15fb3165cd129bd24502085e05200862fa1a49b1
YoadRX <yoadrimok@hilma.tech>
2023-10-31 15:48:02 +0200

    Verifying User Login

--- Client/CheckingLogin.js
+++ Client/CheckingLogin.js
@@ -1,11 +1,24 @@
-function checkValid(){
-    console.log(users);
-    const pwrd = document.querySelector('#pwrd').value;
-    const userName = document.querySelector('#userName').value;
-    const users = localStorage.getItem('users');
-    submitbtn.addEventListener('click',checkingValid)
-        
+const submitbtn = document.querySelector("#submitbtn");
+function checkValid() {
+  const pwrd = document.getElementById("pwrd");
+  const userName = document.getElementById("userName");
+  const users = JSON.parse(localStorage.getItem("database"));
+  const loginForm = document.getElementById("login");
+  function checkValidUP(event) {
+    event.preventDefault();
+    for (let i = 0; i < users.users.length; i++) {
+      const name = users.users[i].username;
+      const password = users.users[i].password;
+      if (name === userName.value && password === pwrd.value) {
+        console.log("niceee");
+        return true;
+      }
+    }
+    console.log("name or password is worng");
+    return false;
+  }
+  loginForm.onsubmit = function (event) {
+    checkValidUP(event);
+  };
 }
-
-
-
+submitbtn.addEventListener("click", checkValid);
